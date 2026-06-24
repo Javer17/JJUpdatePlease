906d4cecc09ca1923ac77504788a8d721cedeeff
Javer17 <jajever17@gmail.com>
2026-06-24 12:47:31 +0200

    Party

--- src/model/political-party.js
+++ src/model/political-party.js
@@ -82,7 +82,7 @@ const defaultMarginNames = {current: "Current", safe: "Safe", likely: "Likely", 
 const PoliticalPartyColors = {
   red: {current: "#600E15", safe: "#be1c29", likely: "#ff5864", lean: "#ff8b98", tilt: "#cf8980"},
   scarlet: {current: "#A82D00", safe: "#DE3B00", likely: "#FF6B36", lean: "#FFAE85", tilt: "#C39A74"},
-  orange: {current: "#A36323", safe: "#ff8000", likely: "#e28c35", lean: "#f8b97a", tilt: "#eec49d"},
+  orange: {current: "#7C3E00", safe: "#FF7000", likely: "#FF9758", lean: "#FFB28B", tilt: "#CFAD80"},
   yellow: {current: "#B86E00", safe: "#FE9800", likely: "#e8a43e", lean: "#eab86e", tilt: "#cbb78a"},
   lime: {current: "#60801B", safe: "#89B30E", likely: "#A1BF47", lean: "#ADBF7C", tilt: "#ACB392"},
   green: {current: "#1f4e11", safe: "#499A21", likely: "#7DBF5C", lean: "#ACD896", tilt: "#A8BF9C"},
@@ -90,7 +90,8 @@ const PoliticalPartyColors = {
   cyan: {current: "#0e2d30", safe: "#1F767A", likely: "#31A7AF", lean: "#6CD2D9", tilt: "#84B2B5"},
   aqua: {current: "#06364d", safe: "#096a97", likely: "#2ba0d6", lean: "#55cfff", tilt: "#73adeb"},
   blue: {current: "#10234E", safe: "#1c408c", likely: "#587ccc", lean: "#8aafff", tilt: "#949bb3"},
-  purple: {current: "#461C81", safe: "#8A38FF", likely: "#B47BFF", lean: "#D3B2FF", tilt: "#B09CBF"},
+  violet: {current: "#461C81", safe: "#8A38FF", likely: "#B47BFF", lean: "#D3B2FF", tilt: "#B09CBF"},
+  purple: {current: "#3D114F", safe: "#7F23A3", likely: "#B700F4", lean: "#DD7FFF", tilt: "#BB9CBF"},
   magenta: {current: "#700036", safe: "#AB0354", likely: "#E8026E", lean: "#fe62b5", tilt: "#B87F9C"},
   pink: {current: "#9b0077", safe: "#ff3e82", likely: "#ff62a9", lean: "#ff9ce5", tilt: "#ffbded"},
   brown: {current: "#2E1500", safe: "#542E11", likely: "#A86234", lean: "#D88856", tilt: "#F7C2A6"},  
@@ -180,7 +181,7 @@ var ReformParty = new PoliticalParty(
   ["Reform", "Reform Party"],
   "Ref",
   "Reform",
-  PoliticalPartyColors.purple
+  PoliticalPartyColors.violet
 )
 
 // Historic Third Parties
@@ -190,7 +191,7 @@ var FreeSoilParty = new PoliticalParty(
   ["Free Soil"],
   "FS",
   "Free Soil",
-  PoliticalPartyColors.purple
+  PoliticalPartyColors.violet
 )
 
 // Independent Candidates
@@ -224,7 +225,7 @@ var Independent2016CPParty = new PoliticalParty(
   ["Powell"],
   "Ind",
   "Powell",
-  PoliticalPartyColors.purple
+  PoliticalPartyColors.violet
 )
 
 var Independent2016BSParty = new PoliticalParty(
@@ -400,7 +401,7 @@ var Independent1892JBParty = new PoliticalParty(
   ["Prohibition"],
   "Ind",
   "Bidwell",
-  PoliticalPartyColors.purple
+  PoliticalPartyColors.violet
 )
 
 var Independent1888CFParty = new PoliticalParty(
@@ -408,7 +409,7 @@ var Independent1888CFParty = new PoliticalParty(
   ["Prohibition"],
   "Ind",
   "Fisk",
-  PoliticalPartyColors.purple
+  PoliticalPartyColors.violet
 )
 
 var Independent1860JohnBreckenridgeParty = new PoliticalParty(
@@ -432,7 +433,7 @@ var Independent1856MFParty = new PoliticalParty(
   ["Know Nothing"],
   "Ind",
   "Fillmore",
-  PoliticalPartyColors.purple
+  PoliticalPartyColors.violet
 )
 
 var Independent1844JBParty = new PoliticalParty(
@@ -440,7 +441,7 @@ var Independent1844JBParty = new PoliticalParty(
   ["Liberty"],
   "Ind",
   "Birney",
-  PoliticalPartyColors.purple
+  PoliticalPartyColors.violet
 )
 
 var Independent1836HWParty = new PoliticalParty(
@@ -448,7 +449,7 @@ var Independent1836HWParty = new PoliticalParty(
   ["Whig"],
   "Ind",
   "White",
-  PoliticalPartyColors.purple
+  PoliticalPartyColors.violet
 )
 
 var Independent1836DWParty = new PoliticalParty(
@@ -668,7 +669,7 @@ var JJULabourConPartyBloc = new PoliticalParty(
   ["LabourCon"],
   "LAC",
   "LabourCon",
-  PoliticalPartyColors.purple
+  PoliticalPartyColors.violet
 )
 
 var JJUWatermelonPartyBloc = new PoliticalParty(
